4b008f4e929487da72b1066233dc6a0c5ed3277e
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2024-05-18 18:40:42 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+var tl=gsap.timeline()
+
+tl.to("#page1,#page2,#page3,#page4,#page5,#page6",{
+    y:"100vh",
+    scale:0.6,
+    duration:0,
+})
+tl.to("#page1,#page2,#page3,#page4,#page5,#page6",{
+    y:"20vh",
+    duration:1,
+    delay:1,
+})
+tl.to("#page1,#page2,#page3,#page4,#page5,#page6",{
+    y:"0vh",
+    rotate:-360,
+    scale:1,
+    duration:2, 
+});
